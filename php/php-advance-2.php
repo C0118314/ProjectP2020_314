08b670d5f9b193984c48691dac46c1f4091b2012
C0118314 <c0118314ff@edu.teu.ac.jp>
2020-06-12 15:07:12 +0900

    php-advance-2再確認

--- php/php-advance-2.php
+++ php/php-advance-2.php
@@ -1,9 +1,10 @@
 <?php
-
-    if(is_uploaded_file($_FILES['file']['tmp_name'])){
-      echo "Successful:ファイルのアップロードに成功しました。";
-    }else{
-      echo "Error:ファイルが指定されていません。";
+    if(isset($_POST["file"])) {
+      if(is_uploaded_file($_FILES["file"]["tmp_name"])){
+        echo "Successful:ファイルのアップロードに成功しました。";
+      }else{
+        echo "Error:ファイルが指定されていません。";
+      }
     }
 ?>
 
@@ -11,16 +12,15 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-<meta charset="UTF-8">
-<title>php-advance-2</title>
+  <meta charset="UTF-8">
+  <title>php-advance-2</title>
 </head>
 <body>
-<h1>ファイルアップローダー</h1>
+  <h1>ファイルアップローダー</h1>
 
-<form enctype="multipart/form-data"  action="php-advance-2.php" method="POST">
-  <input name="file" type="file" />
-  <input type="submit" value="送信" />
-</form>
-
+  <form enctype="multipart/form-data"  action="php-advance-2.php" method="post">
+    <input name="file" type="file" />
+    <input type="submit" value="送信" name="file"/>
+  </form>
 </body>
 </html>
