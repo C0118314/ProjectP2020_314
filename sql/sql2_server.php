a29389e94b7a0f3c14223281ac40566d10034031
C0118314 <c0118314ff@edu.teu.ac.jp>
2020-07-20 22:14:32 +0900

    sql2再提出

--- sql/sql2_server.php
+++ sql/sql2_server.php
@@ -1,19 +1,18 @@
 <?php
+
+
+
 $sql = "select * from addresses";
 $pdo = new PDO(
 'mysql:host=localhost;dbname=address_book;port=3306;
 charset=utf8', c0118314, password);
-$sql1 = $pdo->query($sql);
 
-$addresses = array();
+$addresses = $pdo->query($sql) -> fetchAll(PDO::FETCH_ASSOC);
 
-var_dump($addresses);
-
-foreach ($sql1 as $row) {
-    $addresses[] = $row;
-}
-
-
+//$addresses = array();
+//foreach ($sql1 as $row) {
+//    $addresses[] = $row;
+//}
 $json_string = json_encode($addresses,JSON_UNESCAPED_UNICODE);
 echo $json_string;
 
